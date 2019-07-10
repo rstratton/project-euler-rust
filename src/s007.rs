8b5bb761b390d24e2e359df558c6ae595124d23a
Robert Stratton <r.a.stratton88@gmail.com>
2019-07-10 12:43:19 -0700

    WIP

--- src/s007.rs
+++ src/s007.rs
@@ -9,6 +9,16 @@ struct PrimeMultiple {
     prime: Int,
 }
 
+impl PrimeMultiple {
+    fn new(p: Int) -> PrimeMultiple {
+        PrimeMultiple { next_multiple: p * p, prime: p }
+    }
+
+    fn increment(&mut self) {
+        self.next_multiple += self.prime
+    }
+}
+
 impl Ord for PrimeMultiple {
     fn cmp(&self, other: &PrimeMultiple) -> Ordering {
         other.next_multiple.cmp(&self.next_multiple)
@@ -31,22 +41,40 @@ impl Sieve {
         Sieve { prime_multiples: BinaryHeap::new(), last_prime: 1 }
     }
 
-    fn drain_multiples_eq_to(&mut self, n: Int) {
-        loop {
-            let top = self.prime_multiples.peek();
+    // fn find_next_prime(&mut self) -> Int {
+    //     for num in (self.last_prime + 1).. {
+    //         match self.prime_multiples.peek() {
+    //             Some(PrimeMultiple { next_multiple, .. }) if *next_multiple == num => {
+    //                 self.drain_multiples_eq_to(num);
+    //             },
+    //             _ => {
+    //                 return num;
+    //             }
+    //         }
+    //     };
 
-            match top {
-                Some(PrimeMultiple { next_multiple: m, prime: _ }) => {
-                    if *m != n {
-                        break;
-                    }
-                    let mut multiple = self.prime_multiples.pop().unwrap();
-                    multiple.next_multiple += multiple.prime;
-                    self.prime_multiples.push(multiple);
-                },
-                None => { break; }
+    //     let prime = next_prime;
+    //     self.last_prime = prime;
+    //     self.prime_multiples.push(PrimeMultiple {
+    //         next_multiple: prime * prime,
+    //         prime: prime 
+    //     });
+    //     Some(prime)
+    // }
+
+    fn drain_multiples_eq_to(&mut self, n: Int) {
+        while let Some(PrimeMultiple { next_multiple, .. }) = self.prime_multiples.peek() {
+            if *next_multiple != n {
+                break;
             }
+            let mut multiple = self.prime_multiples.pop().unwrap();
+            multiple.increment();
+            self.prime_multiples.push(multiple);
         }
+    }
+
+    fn insert_new_prime(&mut self, p: Int) {
+        self.prime_multiples.push(PrimeMultiple::new(p));
     }
 }
 
@@ -54,25 +82,24 @@ impl Iterator for Sieve {
     type Item = Int;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut candidate_prime = self.last_prime;
+        let mut candidate_prime = self.last_prime + 1;
 
-        loop {
-            candidate_prime += 1;
-
+        for num in (self.last_prime + 1).. {
             match self.prime_multiples.peek() {
-                Some(PrimeMultiple { next_multiple, .. }) if *next_multiple == candidate_prime => {
-                    self.drain_multiples_eq_to(candidate_prime);
+                Some(PrimeMultiple { next_multiple, .. }) if *next_multiple == num => {
+                    self.drain_multiples_eq_to(num);
                 },
                 _ => {
-                    // If heap is empty (i.e. when candidate_prime is 2), we've found a prime.
                     break;
                 }
             }
-        }
+
+            candidate_prime += 1;
+        };
 
         let prime = candidate_prime;
         self.last_prime = prime;
-        self.prime_multiples.push(PrimeMultiple { next_multiple: prime * prime, prime: prime });
+        self.insert_new_prime(prime);
         Some(prime)
     }
 }
