37c4d549bc28afdbef7871d72eb85090879c226b
Bobby Stratton <r.a.stratton88@gmail.com>
2020-01-08 23:53:46 -0800

    Solve s001 with iterators

--- src/s001.rs
+++ src/s001.rs
@@ -1,11 +1,4 @@
 fn main() {
-    let mut sum = 0;
-
-    for n in 0..1000 {
-        if n % 3 == 0 || n % 5 == 0 {
-            sum += n;
-        }
-    }
-
+    let sum: u32 = (1..1000).filter(|n| n % 3 == 0 || n % 5 == 0).sum();
     println!("{}", sum);
 }
